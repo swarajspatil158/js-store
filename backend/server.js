c90e8b3ac8680bb47080a0ac2d5c5bdb7d9238f8
Swaraj Patil <swarajspatil158@gmail.com>
2022-07-18 21:57:22 +0530

    Add babel to backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const data = require("./data.js");
+import express from "express";
+import cors from "cors";
+import products from "./data.js";
 const app = express();
 app.use(cors());
 app.get("/api/products", (req, res) => {
-  res.send(data.products);
+  res.send(products);
 });
 
 app.listen(5000, () => {
