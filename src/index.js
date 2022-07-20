794ae2a537c342437987d68b66c8fde6eb615542
Swaraj Patil <swarajspatil158@gmail.com>
2022-07-20 18:36:57 +0530

    Create localstorage to add items

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import parseRequestUrl from './utils'
 import Error404Screen from './screens/Error404Screen'
+import CartScreen from './screens/CartScreen';
 
 const routes = {
     '/': HomeScreen,
     '/product/:id': ProductScreen,
+    '/cart/:id': CartScreen,
+    '/cart':CartScreen
 }
 const router = async () => {
     const request = parseRequestUrl()
@@ -15,8 +18,8 @@ const router = async () => {
         (request.verb ? `/${request.verb}` : '')
     const screen = routes[parseUrl] ? routes[parseUrl] : Error404Screen
 
-    const main = document.getElementById('main-container')
-    main.innerHTML = await screen.render()
+    const main = document.getElementById('main-container');
+    main.innerHTML = await screen.render();
     await screen.after_render();
 }
 window.addEventListener('load', router)
